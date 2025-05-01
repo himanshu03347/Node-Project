5f4c1460f2f1eacb39227e366cc27aad8575f9d2
Himanshu Goyal <himanshu03347@gmail.com>
2025-05-01 11:01:45 +0530

    Addition of basic express funtionalities

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const { serverConfig, Logger } = require('./config');
+const apiRoutes = require('./routes');
+
+const app = express();
+
+app.use('/api', apiRoutes);
+
+app.listen(serverConfig.PORT, ()=>{
+    console.log(`Successfully start the server on PORT: ${serverConfig.PORT}`);
+    // Logger.info("Succesfully started the server", "root", {});
+});
